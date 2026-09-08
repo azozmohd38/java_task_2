9c36b096a71bdf720dddd1e4acf87da8aad70848
Abdulaziz Mohammed <azozmohd38@gmail.com>
2026-09-08 15:04:13 +0400

    Refine task27 structure 8/10

--- Count Even and Odd Numbers/task27.java
+++ Count Even and Odd Numbers/task27.java
@@ -19,6 +19,7 @@ public class task27 {
         for (int i = start; i <= end; i++) {
 
             // Check whether the number is even or odd
+
             if (i % 2 == 0) {
                 evenCount++;
             } else {
