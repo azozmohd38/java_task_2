b9a2318be5bb1885b425bf8d17b0ac9db2dcc2bc
Abdulaziz Mohammed <azozmohd38@gmail.com>
2026-09-08 15:07:18 +0400

    Finish task

--- Number Reversal/task22.java
+++ Number Reversal/task22.java
@@ -26,6 +26,7 @@ public class task22 {
             reversedNumber = (reversedNumber * 10) + digit;
 
             // Remove the last digit from the original number
+
             number = number / 10;
         }
 
