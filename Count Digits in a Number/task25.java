5b981298e864c0bb78431cbac46853dbf348d292
Abdulaziz Mohammed <azozmohd38@gmail.com>
2026-09-08 15:03:50 +0400

    Refine task25 structure 3/10

--- Count Digits in a Number/task25.java
+++ Count Digits in a Number/task25.java
@@ -4,6 +4,7 @@ public class task25 {
     public static void main(String[] args) {
 
         // Declare and initialize the required variables
+
         int number = 123456;
         int count = 0;
 
