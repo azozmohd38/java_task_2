6871aa5e211a69e1df9fdfb2e65feed676c9fcba
Abdulaziz Mohammed <azozmohd38@gmail.com>
2026-09-08 15:08:17 +0400

    Update average

--- Student Grade Calculator/task24.java
+++ Student Grade Calculator/task24.java
@@ -4,6 +4,7 @@ public class task24 {
     public static void main(String[] args) {
 
         // Declare and initialize the grades for three subjects
+
         double grade1 = 85;
         double grade2 = 90;
         double grade3 = 80;
