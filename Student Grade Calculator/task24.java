02d577c68da537cc8e182e96e8d9f703d265d6bc
Abdulaziz Mohammed <azozmohd38@gmail.com>
2026-09-08 15:08:56 +0400

    Finish task

--- Student Grade Calculator/task24.java
+++ Student Grade Calculator/task24.java
@@ -23,6 +23,7 @@ public class task24 {
         // Determine the student's performance using conditional statements
 
         if (average >= 90) {
+
             status = "Excellent";
         } else if (average >= 75) {
             status = "Good";
